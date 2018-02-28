e47d8cba0c8abb6a5a39f30fbcf564f7313d4d55
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-28 14:48:09 +1100

    LO, Simon: 2018-02-28 14:48:09

--- user/Calculator.py
+++ user/Calculator.py
@@ -1,5 +1,11 @@
-number = input ('')
-number = int (number)
-numbers2 = input ('')
-number2 = int (number2)
-print (number * number2)
+"""
+Program: Calculator
+Name: Simon Lo
+Date: 15/02/18
+Desc: to calculate
+"""
+import random
+nums = [x for x in range(10)]
+random.shuffle(nums)
+nums
+[6, 3, 5, 4, 0, 1, 2, 9, 8, 7]
--- user/Magic8Ball.py
+++ user/Magic8Ball.py
@@ -1,5 +1,29 @@
-import random
-nums = [x for x in range(10)]
-random.shuffle(nums)
-nums
-[6, 3, 5, 4, 0, 1, 2, 9, 8, 7]
+"""
+Program: Magic 8 ball
+Name: Simon Lo
+Date: 21/02/18
+Desc: to randomized the answer
+"""
+
+ans = 'true'
+while ans:
+    qusetion = ("Ask the magic 8 ball a qusetion: (press enter to quit)")
+    answers = random.randint(1,8)
+    if qusetion == "":
+        sys.exit()
+    if answer == 1:
+        print ("Yes, that is a good idea")
+    if anwer == 2:
+        print ("No, that is a horrible choice")
+    if answer == 3:
+        print ("I am not sure if that is the best idea")
+    if answer == 4:
+        print ("This is your own decision to make, I will belive on your choice")
+    if answer == 5:
+        print ("You should pick a different choice")
+    if answer == 6:
+        print ("The choice you have chosen is the best for you")
+    if answer == 7:
+        print ("Chould you ask the question again")
+    if answer == 8:
+         print ("Don't ask me") 
