1b55eb9527695689f74427ab0b062ea06cdea2dd
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-03-01 12:29:34 +1100

    LO, Simon: 2018-03-01 12:29:34

--- user/Hello World.py
+++ user/Hello World.py
@@ -1,2 +1,8 @@
-print('Hello, world!')
-print('Hello, world! My name is')
+"""
+Program: Hello world
+Name: Simon Lo
+Date: 15/02/2018
+Desc: To say hello
+"""
+name=input('')
+print ('Hello', name, 'my name is Vicent')
---
+++ user/my thing.py
@@ -0,0 +1,4 @@
+x = 1
+while x > 0:
+    x = x * 2
+    print(x)
