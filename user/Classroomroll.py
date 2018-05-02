182d8ba27d4c5abaccb967a2caf69c80a102ebb3
Simon Lo <lo-0006@nhs.vic.edu.au>
2018-05-02 14:31:25 +1000

    Classroom Roll

---
+++ user/Classroomroll.py
@@ -0,0 +1,13 @@
+"""
+Prog:Class roll
+Name:Simon Lo
+Date:2/05/18
+Desc:to see everybody is here
+"""
+roll = ['Jessica', 'Emily', 'Jordan', 'Kayley', 'Bruce', 'Michael', 'Everett', 'Lisa', 'Sam', 'Noah']
+student1= roll[2]
+print(student1)
+enrolment= len(roll)
+roll.append('James')
+del (roll[2])
+roll[5]='Mike'
